85338ac87121a3b14f0f7c58a4c778a53d0bf28b
hongghe <364930855@qq.com>
2018-08-20 20:33:54 +0800

    Initial commit

---
+++ src/main/java/com/hongghe/algorithmexamples/AlgorithmExamplesApplication.java
@@ -0,0 +1,12 @@
+package com.hongghe.algorithmexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmExamplesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hongghe/algorithmexamples/AlgorithmExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hongghe.algorithmexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlgorithmExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
