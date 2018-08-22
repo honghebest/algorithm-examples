f8ffb64b6f0d12baf847f08e34d70ef9ac5dfe49
hongghe <364930855@qq.com>
2018-08-22 14:07:26 +0800

    二叉树的操作。

---
+++ src/main/java/com/hongghe/algorithmexamples/dataconstruction/array/ArrayEntity.java
@@ -0,0 +1,10 @@
+package com.hongghe.algorithmexamples.dataconstruction.array;
+
+/**
+ * @author hongghe 2018/8/22
+ */
+public class ArrayEntity {
+    public Object element;
+    public ArrayEntity frontArrayEntity;
+    public ArrayEntity backArrayEntity;
+}
--- src/main/java/com/hongghe/algorithmexamples/dataconstruction/tree/BinaryTree.java
+++ src/main/java/com/hongghe/algorithmexamples/dataconstruction/tree/BinaryTree.java
@@ -22,4 +22,61 @@ public class BinaryTree {
             return leftDepth > rightDepth ? leftDepth : rightDepth;
         }
     }
+
+    /**
+     * 获取第n层节点的个数
+     *
+     * @param entity 二叉树的结构
+     * @param level 第n层
+     * @return 节点的个数
+     */
+    public int getLevelNoeAmount(BinaryTreeEntity entity, int level) {
+        if (entity == null || level < 1) {
+            return 0;
+        } else {
+            if (level == 1) {
+                return 1;
+            } else {
+                return getLevelNoeAmount(entity.leftChild, level-1) +
+                        getLevelNoeAmount(entity.rightChild, level-1);
+            }
+        }
+    }
+
+    /**
+     * 比较二叉树的左右的结构是否一样
+     *
+     * @param leftEntity 二叉树的左结构
+     * @param rightEntity 二叉树的右结构
+     * @return 是否一样
+     */
+    public boolean compareBTreeConstruct(BinaryTreeEntity leftEntity, BinaryTreeEntity rightEntity) {
+        if (leftEntity == null && rightEntity == null) {
+            return true;
+        } else if (leftEntity == null || rightEntity == null) {
+            return false;
+        }
+        boolean resultLeft = compareBTreeConstruct(leftEntity.leftChild, leftEntity.rightChild);
+        boolean resultRight = compareBTreeConstruct(rightEntity.leftChild, rightEntity.rightChild);
+        return resultLeft && resultRight;
+    }
+
+    /**
+     * 求一个二叉树的镜像
+     *
+     * @param entity 所求二叉树的结构
+     * @return 镜像二叉树
+     */
+    public BinaryTreeEntity mirror(BinaryTreeEntity entity) {
+        if (entity == null) {
+            return null;
+        } else {
+            BinaryTreeEntity binaryTreeEntity = entity.leftChild;
+            entity.leftChild = entity.getRightChild();
+            entity.rightChild = binaryTreeEntity;
+            mirror(entity.leftChild);
+            mirror(entity.rightChild);
+        }
+        return entity;
+    }
 }
