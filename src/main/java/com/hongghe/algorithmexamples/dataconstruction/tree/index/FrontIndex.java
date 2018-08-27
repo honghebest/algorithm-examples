500a61da0202346f8d543e33069a3a9a4074cd1b
hongghe <364930855@qq.com>
2018-08-27 23:21:27 +0800

    index .

--- src/main/java/com/hongghe/algorithmexamples/dataconstruction/tree/index/FrontIndex.java
+++ src/main/java/com/hongghe/algorithmexamples/dataconstruction/tree/index/FrontIndex.java
@@ -1,7 +1,51 @@
 package com.hongghe.algorithmexamples.dataconstruction.tree.index;
 
+import lombok.Data;
+
+import java.util.Stack;
+
 /**
  * @author hongghe 2018/8/27
  */
 public class FrontIndex {
+
+    @Data
+    static class Node {
+        public String data;
+        public Node leftNode;
+        public Node rightNode;
+    }
+
+    public void index(Node root) {
+        if (root == null) {
+            return;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        Node currentNode = new Node();
+        Node lastVisitNode = new Node();
+        currentNode = root;
+        lastVisitNode= null;
+
+        while (currentNode != null) {
+            stack.push(currentNode);
+            currentNode = currentNode.getLeftNode();
+        }
+
+        while (!stack.empty()) {
+            currentNode = stack.pop();
+            if (currentNode.getRightNode() != null &&
+                    currentNode.getRightNode() != lastVisitNode) {
+                stack.push(currentNode);
+                currentNode.getRightNode();
+                while (currentNode != null) {
+                    stack.push(currentNode);
+                    currentNode = currentNode.getLeftNode();
+                }
+            } else {
+                System.out.println(currentNode.getData());
+                lastVisitNode = currentNode;
+            }
+        }
+    }
 }
