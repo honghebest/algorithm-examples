2bf51c5174d8ea554f0b0800fcd9bf831d2a6cfc
hongghe <364930855@qq.com>
2018-08-27 20:32:20 +0800

    增加算法。

---
+++ src/main/java/com/hongghe/algorithmexamples/algorithm/sort/javasourcesort/ArraySort.java
@@ -0,0 +1,19 @@
+package com.hongghe.algorithmexamples.algorithm.sort.javasourcesort;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author hongghe 2018/8/26
+ */
+public class ArraySort {
+
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList("6", "1", "3", "1","2");
+        Collections.sort(strings);
+        for (String string : strings) {
+            System.out.println(string);
+        }
+    }
+}
---
+++ src/main/java/com/hongghe/algorithmexamples/algorithm/sort/javasourcesort/CollectionSort.java
@@ -0,0 +1,14 @@
+package com.hongghe.algorithmexamples.algorithm.sort.javasourcesort;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author hongghe 2018/8/26
+ */
+public class CollectionSort {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("6", "1", "3", "1","2");
+    }
+}
