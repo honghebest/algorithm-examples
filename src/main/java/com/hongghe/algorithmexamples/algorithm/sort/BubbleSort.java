858852ca48b7ffe7e6e36857490ba360091436a4
hongghe <364930855@qq.com>
2018-09-11 20:15:50 +0800

    Main method.

--- src/main/java/com/hongghe/algorithmexamples/algorithm/sort/BubbleSort.java
+++ src/main/java/com/hongghe/algorithmexamples/algorithm/sort/BubbleSort.java
@@ -1,10 +1,15 @@
 package com.hongghe.algorithmexamples.algorithm.sort;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author hongghe 2018/8/24
  */
 public class BubbleSort<T extends Comparable<T>> extends Sort<T> {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(BubbleSort.class);
+    
     @Override
     public void sort(Comparable[] nums) {
 
