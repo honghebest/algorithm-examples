8864e7ae1cbc656814d5272870a0f459b7983b79
hongghe <364930855@qq.com>
2018-12-25 20:47:35 +0800

    binary tree unrecursion research.

---
+++ src/main/java/com/hongghe/algorithmexamples/algorithm/search/BinaryTreeUnrecursionSearch.java
@@ -0,0 +1,23 @@
+package com.hongghe.algorithmexamples.algorithm.search;
+
+import com.hongghe.algorithmexamples.algorithm.binarytree.BTreeNode;
+
+/**
+ * 二叉树的非递归的遍历的实现
+ *
+ * @author hongghe 2018/12/25
+ */
+public class BinaryTreeUnrecursionSearch {
+
+    /**
+     * 非递归的前续遍历
+     *
+     * @param bTreeNode 开始节点
+     */
+    public static void preOrder(BTreeNode bTreeNode) {
+        if (bTreeNode == null) {
+            return;
+        }
+
+    }
+}
