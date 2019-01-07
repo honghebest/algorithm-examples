c8bff061cd317054e4cdd31137e40db37f0fd6f1
hongghe <364930855@qq.com>
2019-01-07 21:22:06 +0800

    The setting.

--- src/main/java/com/hongghe/algorithmexamples/data/construction/set/SetEntity.java
+++ src/main/java/com/hongghe/algorithmexamples/data/construction/set/SetEntity.java
@@ -1,7 +1,14 @@
 package com.hongghe.algorithmexamples.data.construction.set;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author hongghe 2018/8/22
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SetEntity {
 }
