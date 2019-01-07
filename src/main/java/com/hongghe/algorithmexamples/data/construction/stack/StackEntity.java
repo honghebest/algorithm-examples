ccdf74ebb2d605bf55b00a8f756fdfd1f5f3bd07
hongghe <364930855@qq.com>
2019-01-07 21:06:51 +0800

    several constants setting.

--- src/main/java/com/hongghe/algorithmexamples/data/construction/stack/StackEntity.java
+++ src/main/java/com/hongghe/algorithmexamples/data/construction/stack/StackEntity.java
@@ -1,7 +1,20 @@
 package com.hongghe.algorithmexamples.data.construction.stack;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author hongghe 2018/8/22
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class StackEntity {
+
+    /**
+     * The data.
+     */
+    public Object object;
+
 }
