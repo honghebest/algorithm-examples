c7616bf383da2a5fa6f1711563a406a9b0717040
hongghe <364930855@qq.com>
2019-02-12 19:58:59 +0800

    several constants setting.

---
+++ src/main/java/com/hongghe/algorithmexamples/data/construction/queue/SelfQueueOperation.java
@@ -0,0 +1,10 @@
+package com.hongghe.algorithmexamples.data.construction.queue;
+
+/**
+ * @author: hongghe @date: 2019-02-12 16:57
+ */
+public class SelfQueueOperation {
+
+    SelfQueueOperation() {}
+
+}
