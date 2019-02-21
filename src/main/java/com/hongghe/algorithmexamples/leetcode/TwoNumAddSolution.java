d36b56f57b55ae650763d579408ad8ed791e9350
hongghe <364930855@qq.com>
2019-02-21 10:12:26 +0800

    several constants setting.

---
+++ src/main/java/com/hongghe/algorithmexamples/leetcode/TwoNumAddSolution.java
@@ -0,0 +1,7 @@
+package com.hongghe.algorithmexamples.leetcode;
+
+/**
+ * @author: hongghe @date: 2019-02-21 10:10
+ */
+public class TwoNumAddSolution {
+}
---
+++ src/main/java/com/hongghe/algorithmexamples/leetcode/TwoNumSumSolution.java
@@ -0,0 +1,7 @@
+package com.hongghe.algorithmexamples.leetcode;
+
+/**
+ * @author: hongghe @date: 2019-02-21 10:09
+ */
+public class TwoNumSumSolution {
+}
