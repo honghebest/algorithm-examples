b2de6580d4f8ed6fdd333d3bd17bb06d3d4189cf
hongghe <364930855@qq.com>
2019-02-21 19:21:30 +0800

    several constants setting.

--- src/main/java/com/hongghe/algorithmexamples/leetcode/TwoNumAddSolution.java
+++ src/main/java/com/hongghe/algorithmexamples/leetcode/TwoNumAddSolution.java
@@ -1,7 +1,34 @@
 package com.hongghe.algorithmexamples.leetcode;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author: hongghe @date: 2019-02-21 10:10
  */
 public class TwoNumAddSolution {
+
+      public class ListNode {
+          int val;
+          ListNode next;
+         ListNode(int x) { val = x; }
+      }
+
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+          List<Integer> list = new LinkedList<>();
+          while (l1.next != null) {
+             ((LinkedList<Integer>) list).push(l1.val + l2.val);
+              l1 = l2.next;
+              l2 = l2.next;
+          }
+
+        ListNode listNode = new ListNode(0);
+        for (int i=0; i<list.size(); i++) {
+            listNode.val = list.get(list.size()-i-1);
+            listNode = listNode.next;
+        }
+        return listNode;
+    }
+
 }
