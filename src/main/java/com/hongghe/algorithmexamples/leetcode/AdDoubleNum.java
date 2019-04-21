cb027f26c72568a9470cc6fa231353b8e70eeeb9
hongghe <364930855@qq.com>
2019-04-21 22:25:23 +0800

    fibonacci.

---
+++ src/main/java/com/hongghe/algorithmexamples/leetcode/AdDoubleNum.java
@@ -0,0 +1,34 @@
+package com.hongghe.algorithmexamples.leetcode;
+
+import com.hongghe.algorithmexamples.leetcode.domain.ListNode;
+
+/**
+ * Leetcode:给定两个非空链表来表示两个非负整数。位数按照逆序方式存储，它们的每个节点只存储单个数字。将两数相加返回一个新的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ *
+ * @author: hongghe @date: 2019-04-21 22:19
+ */
+public class AdDoubleNum {
+
+    public ListNode addTwo(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum/10;
+            curr.next = new ListNode(sum%10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+}
